af0d8905c62761234cbdb9219872225914d63548
Maxime Vidori <maxime.vidori@gmail.com>
2017-09-15 16:20:45 +0200

    Support for sass, add timeout on reload, various fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,21 @@ var path = require('path');
 var hb = require('gulp-hb');
 var obj = require('through2').obj;
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var del = require('del');
 
 var paths = {
-  src: path.join('src', '**'),
+  src: path.join('src', '**', '*.scss'),
   build: {
-    base: path.join('build', '**'),
+    base: path.join('build'),
     static: path.join('build', 'static'),
     books: path.join('build', 'books'),
     series: path.join('build', 'series'),
     css: path.join('build', 'static', 'css')
   },
   assets: {
-    static: path.join('assets', 'static', '**'),
-    base: path.join('assets', '**'),
+    base: path.join('assets'),
+    static: path.join('assets', 'static'),
     pages: path.join('assets', 'pages', '*.html'),
     partials: path.join('assets', 'partials', '*.hbs'),
     helpers: path.join('assets', 'helpers', '*.js'),
@@ -26,19 +28,32 @@ var paths = {
 };
 
 gulp.task('watch', ['templates', 'copy', 'build'], function () {
+  var last = (new Date).getTime();
   gulp.watch(paths.src, ['build']);
-  gulp.watch(paths.assets.base, ['templates']);
-  gulp.watch(paths.assets.static, ['copy']);
-  gulp.watch(paths.build.base, ['reload']);
+  gulp.watch(path.join(paths.assets.base, '**', '*'), ['templates']);
+  gulp.watch(path.join(paths.assets.static, '**', '*') ['copy']);
+  gulp.watch(path.join(paths.build.base, '**', '*'), function(event) {
+    var current = (new Date).getTime();
+    if (current - last > 1000) {
+      gulp.start('reload');
+      last = current;
+    }
+  });
 });
 
+gulp.task('clean', function() {
+  return del(paths.build.base);
+});
 
-gulp.task('build', function() {
-  gulp.src(paths.src).pipe(gulp.dest(paths.build.css));
+gulp.task('build', function () {
+  return gulp.src(paths.src)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(paths.build.css));
 });
 
 gulp.task('copy', function() {
-  gulp.src(paths.assets.static).pipe(gulp.dest(paths.build.static))
+  return gulp.src(path.join(paths.assets.static, '**', '*'))
+    .pipe(gulp.dest(paths.build.static));
 })
 
 gulp.task('reload', function () {
@@ -54,7 +69,7 @@ gulp.task('templates:pages', function () {
     data: paths.assets.data,
     helpers: paths.assets.helpers
   }))
-  .pipe(gulp.dest(path.dirname(paths.build.base)));
+  .pipe(gulp.dest(paths.build.base));
 });
 
 gulp.task('templates:books', function() {
@@ -109,7 +124,7 @@ gulp.task('templates:series', function () {
 
 gulp.task('serve', ['watch'], function () {
   connect.server({
-    root: path.dirname(paths.build.base),
+    root: paths.build.base,
     livereload: true,
     host: '0.0.0.0',
     port: 8000
