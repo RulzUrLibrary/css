935658487ad6be25b5a1cef9a9ce13c50a74784b
Paul Bismuth <paul.bismuth.f@gmail.com>
2017-11-09 11:01:38 +0100

    Incorporate old css project

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,35 @@ var sass = require('gulp-sass');
 var del = require('del');
 
 var paths = {
-  src: path.join('src', '**', '*.scss'),
-  build: {
-    base: path.join('build'),
-    static: path.join('build', 'static'),
-    books: path.join('build', 'books'),
-    series: path.join('build', 'series'),
-    css: path.join('build', 'static', 'css')
+  src: {
+    base:   path.join('src'),
+    scss:   path.join('src', 'main.scss'),
+    fonts:  path.join('src', 'fonts')
   },
-  assets: {
-    base: path.join('assets'),
-    static: path.join('assets', 'static'),
-    pages: path.join('assets', 'pages', '*.html'),
-    partials: path.join('assets', 'partials', '*.hbs'),
-    helpers: path.join('assets', 'helpers', '*.js'),
-    generated: path.join('assets', 'generated'),
-    data: path.join('assets', 'data', '*.{js,json}')
+  dist: {
+    base:    path.join('dist'),
+    static:  path.join('dist', 'static'),
+    books:   path.join('dist', 'books'),
+    series:  path.join('dist', 'series'),
+    css:     path.join('dist', 'static', 'css')
+  },
+  docs: {
+    base:       path.join('docs'),
+    static:     path.join('docs', 'static'),
+    pages:      path.join('docs', 'pages', '*.html'),
+    partials:   path.join('docs', 'partials', '*.hbs'),
+    helpers:    path.join('docs', 'helpers', '*.js'),
+    generated:  path.join('docs', 'generated'),
+    data:       path.join('docs', 'data', '*.{js,json}')
   }
 };
 
-gulp.task('watch', ['templates', 'copy', 'build'], function () {
+gulp.task('watch', ['templates', 'fonts', 'copy', 'build'], function () {
   var last = (new Date).getTime();
-  gulp.watch(paths.src, ['build']);
-  gulp.watch(path.join(paths.assets.base, '**', '*'), ['templates']);
-  gulp.watch(path.join(paths.assets.static, '**', '*') ['copy']);
-  gulp.watch(path.join(paths.build.base, '**', '*'), function(event) {
+  gulp.watch(path.join(paths.src.base, '**', '*'), ['build']);
+  gulp.watch(path.join(paths.docs.base, '**', '*'), ['templates']);
+  gulp.watch(path.join(paths.docs.static, '**', '*') ['copy']);
+  gulp.watch(path.join(paths.dist.base, '**', '*'), function(event) {
     var current = (new Date).getTime();
     if (current - last > 1000) {
       gulp.start('reload');
@@ -42,42 +46,47 @@ gulp.task('watch', ['templates', 'copy', 'build'], function () {
 });
 
 gulp.task('clean', function() {
-  return del(paths.build.base);
+  return del(paths.dist.base);
 });
 
 gulp.task('build', function () {
-  return gulp.src(paths.src)
+  return gulp.src(paths.src.scss)
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest(paths.build.css));
+    .pipe(gulp.dest(paths.dist.css));
 });
 
 gulp.task('copy', function() {
-  return gulp.src(path.join(paths.assets.static, '**', '*'))
-    .pipe(gulp.dest(paths.build.static));
+  return gulp.src(path.join(paths.docs.static, '**', '*'))
+    .pipe(gulp.dest(paths.dist.static));
+})
+
+gulp.task('fonts', function() {
+  return gulp.src(path.join(paths.src.fonts, '**', '*'), {base: paths.src.base})
+    .pipe(gulp.dest(paths.dist.css));
 })
 
 gulp.task('reload', function () {
-  gulp.src(paths.build.base).pipe(connect.reload());
+  gulp.src(paths.dist.base).pipe(connect.reload());
 });
 
 gulp.task('templates', ['templates:pages', 'templates:books', 'templates:series']);
 gulp.task('templates:pages', function () {
   return gulp
-  .src(paths.assets.pages)
+  .src(paths.docs.pages)
   .pipe(hb({
-    partials: paths.assets.partials,
-    data: paths.assets.data,
-    helpers: paths.assets.helpers
+    partials: paths.docs.partials,
+    data: paths.docs.data,
+    helpers: paths.docs.helpers
   }))
-  .pipe(gulp.dest(paths.build.base));
+  .pipe(gulp.dest(paths.dist.base));
 });
 
 gulp.task('templates:books', function() {
-  var parts = ['.', path.dirname(paths.assets.data), 'books.json'];
+  var parts = ['.', path.dirname(paths.docs.data), 'books.json'];
   var books = require(parts.join(path.sep));
 
   return gulp
-  .src(path.join(paths.assets.generated, 'book.html'))
+  .src(path.join(paths.docs.generated, 'book.html'))
   .pipe(obj(function(file, _, cb) {
     var that = this;
     books.forEach(function(book) {
@@ -91,18 +100,18 @@ gulp.task('templates:books', function() {
     cb();
   }))
   .pipe(hb({
-    partials: paths.assets.partials,
-    helpers: paths.assets.helpers
+    partials: paths.docs.partials,
+    helpers: paths.docs.helpers
   }))
-  .pipe(gulp.dest(paths.build.books));
+  .pipe(gulp.dest(paths.dist.books));
 });
 
 gulp.task('templates:series', function () {
-  var parts = ['.', path.dirname(paths.assets.data), 'series_detail.json'];
+  var parts = ['.', path.dirname(paths.docs.data), 'series_detail.json'];
   var series = require(parts.join(path.sep));
 
   return gulp
-  .src(path.join(paths.assets.generated, 'serie.html'))
+  .src(path.join(paths.docs.generated, 'serie.html'))
   .pipe(obj(function(file, _, cb) {
     var that = this;
     series.forEach(function(serie) {
@@ -116,15 +125,15 @@ gulp.task('templates:series', function () {
     cb();
   }))
   .pipe(hb({
-    partials: paths.assets.partials,
-    helpers: paths.assets.helpers
+    partials: paths.docs.partials,
+    helpers: paths.docs.helpers
   }))
-  .pipe(gulp.dest(paths.build.series));
+  .pipe(gulp.dest(paths.dist.series));
 });
 
 gulp.task('serve', ['watch'], function () {
   connect.server({
-    root: paths.build.base,
+    root: paths.dist.base,
     livereload: true,
     host: '0.0.0.0',
     port: 8000
